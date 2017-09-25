b1f91f23fc22a5b0d876a35c2600db350ebfcf28
Sebastian Webber <sebastian@swebber.me>
2017-09-25 16:09:32 -0300

    changing config to use logger

--- cmd/root.go
+++ cmd/root.go
@@ -97,6 +97,6 @@ func initConfig() {
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
+		log.Info("Using config file: " + viper.ConfigFileUsed())
 	}
 }
